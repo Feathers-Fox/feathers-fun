1b74f7a2cecf4f1af767557d84feb3e1aaf6729b
Feathers-Fox <ariannabanta12@gmail.com>
2017-06-19 11:24:57 -0500

    Add files via upload

--- proj01/proj01.py
+++ proj01/proj01.py
@@ -1,10 +1,32 @@
-# Name:
-# Date:
+# Name:Arianna
+# Date:6/19/17
 
 # proj01: A Simple Program
 # This program asks the user for his/her name and age.
 # Then, it prints a sentence that says when the user will turn 100.
 
 # If you complete extensions, describe your extensions here!
+
+
+print "programming"
+print "fox"
+
+
+a=raw_input("what is your name")
+
+print a
+
+b=raw_input("what is your age")
+
+print b
+
+b= int(b)
+d= 2017
+
+print d-b+100
+
+
+
+
 
 
