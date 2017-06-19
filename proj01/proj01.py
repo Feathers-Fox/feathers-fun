468a6c2b104649fc10bc351e89d054bfdaad316a
Feathers-Fox <ariannabanta12@gmail.com>
2017-06-19 15:33:48 -0500

    Add files via upload

--- proj01/proj01.py
+++ proj01/proj01.py
@@ -11,17 +11,56 @@
 print "programming"
 print "fox"
 
-
+d=2017
+d=int(d)
 a=raw_input("what is your name")
 
 print a
 
+
+e=raw_input("have you already had your birthday this year (yes or no)")
+
+print e
+
+ l=raw_input("yes")
+
 b=raw_input("what is your age")
+
 
 print b
 
 b= int(b)
-d= 2017
+
+if b>=17:
+    print "you may watch G, PG, PG-13, and R rated movies"
+
+if b<=12:
+    print "you can only watch G, and PG movies"
+
+if b == 13:
+    print "you can only watch G, PG, and PG-13 movies"
+
+if b ==14:
+    print "you can only watch G, PG, and PG-13 movies"
+
+if b ==15:
+    print "you can only watch G, PG, and PG-13 movies"
+
+if b==16:
+    print "you can only watch G, PG, and PG-13 movies"
+
+else:
+    print"you can't watch R rated movies"
+
+
+
+    b= int(b)
+
+d= int(d)
+
+l=raw_input("yes")
+
+
 
 print d-b+100
 
--- proj02/proj02_01.py
+++ proj02/proj02_01.py
@@ -7,16 +7,27 @@
 
 # If you complete extensions, describe your extensions here!
 
-name = raw_input("Enter your name: ")
+name = raw_input("Enter your name please!: ")
+
+print name
+
 age = int(raw_input("Enter your age: "))
+
+print age
+
 birthday = raw_input("Has your birthday happened this year? Enter Y or N: ")
 
 if birthday == "Y":
-    # Calculates the year that the user will be 100
     year_100 = str((100 - age) + 2017)
 
 else:
     # Calculates the year that the user will be 100
     year_100 = str((100 - age) + 2016)
 
+
 print name, " will turn 100 in the year ", year_100, "."
+
+year_100=int(year_100)
+
+for variable in range (2017-age, year_100+1 ):
+    print "counter is now", variable
