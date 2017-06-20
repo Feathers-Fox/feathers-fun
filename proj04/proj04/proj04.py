db26c9713ab1a195cc384d1f7518f9842be673f6
Feathers-Fox <ariannabanta12@gmail.com>
2017-06-20 15:27:28 -0500

    Add files via upload

---
+++ proj04/proj04/proj04.py
@@ -0,0 +1,33 @@
+# Name:
+# Date:
+
+"""
+proj04
+
+Asks the user for a string and prints out whether or not the string is a palindrome.
+
+"""
+word=raw_input("Type in a word, please!")
+
+listA= []
+for letters in word:
+    listA.append(letters)
+print listA
+
+#listA=listA[' ' -1]
+
+for letters in listA:
+
+
+    #listA=listA[-1]
+    print listA
+    listA=listA[-1]
+
+    if listA in letters:
+
+        print 
+
+
+
+
+
