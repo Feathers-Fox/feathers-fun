df3ae6ee8f5746c12a6f5b32e690dd95411642f8
Feathers-Fox <ariannabanta12@gmail.com>
2017-06-20 14:35:26 -0500

    Add files via upload

--- proj03/proj03.py
+++ proj03/proj03.py
@@ -12,4 +12,42 @@ Keep track of how many guesses the user has taken, and when the game ends, print
 
 """
 
+guesses = -1
+while guesses < 6:
+    guesses = guesses + 1
+    print "you have played this game"
+    print guesses
+    print "times so far!"
+    import random
+
+    var = random.randint(1, 9)
+    #print var
+
+    guess = int(raw_input("Pick a number... any number (only 1-9 though)"))
+    if guess > 9 or guess < 1:
+        guess2 = int(raw_input("maybe you didnt hear me. only numbers one through nine!"))   # correct
+        if guess2 > 9 or guess2 < 1:
+            print "... i SAID any number BETWEEN ONE AND NINE"
+
+            # if guess2 > 9 or guess2 < 1:
+        print "...if you arent going to play this game right, why are you here? (***roasted***) and since you were either that stupid or just that much of a brat, you cant play anymore."
+        print "goodbye world"
+        exit()
+    if guess == int(var):
+        print "ya got it! thats the correct number!"
+
+
+
+    else:
+        print str(var)
+        print "...was the correct randomly generated answer. sorry! you didnt win. try again, or press control --> D to quit"
+    if guess > int(var):
+        print "a little too high :|"
+    if guess < int(var):
+        print "a little too low :|"
+
+
+
+
+
 
